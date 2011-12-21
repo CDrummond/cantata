8b3ada46c82445dea7bf79d63430e65d7627bf35
craig <craig.p.drummond@gmail.com>
2011-12-21 19:45:47 +0000

    Use WebKit to display artist/albumm info

--- gui/infopage.h
+++ gui/infopage.h
@@ -3,21 +3,20 @@
  *
  * Copyright (c) 2011 Craig Drummond <craig.p.drummond@gmail.com>
  *
- */
-/* BE::MPC Qt4 client for MPD
- * Copyright (C) 2010-2011 Thomas Luebking <thomas.luebking@web.de>
+ * ----
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License version 2 as published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU Library General Public License
- * along with this library; see the file COPYING.LIB.  If not, write to
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
  */
@@ -28,8 +27,13 @@
 #include <QWidget>
 #include "song.h"
 
-class QTextBrowser;
+#ifdef ENABLE_KDE_SUPPORT
+#define WebView KWebView
+#else
+#define WebView QWebView
+#endif
 class QAction;
+class WebView;
 
 class InfoPage : public QWidget
 {
@@ -41,19 +45,18 @@ public:
     void update(const Song &s);
 
 private:
-    void fetchInfo(bool useCache=true);
-    void askGoogle(const QString &query, bool useCache=true);
+    void fetchInfo();
+    void askGoogle(const QString &query);
     void fetchWiki(QString query);
 
 private Q_SLOTS:
     void refresh();
     void changeView();
-    void gg2wp(const QString &answer);
-    void setArtistWiki(const QString &artistWiki);
+    void googleAnswer(const QString &answer);
 
 private:
     QAction *refreshAction;
-    QTextBrowser *text;
+    WebView *view;
     QComboBox *combo;
     QString lastWikiQuestion;
     Song song;

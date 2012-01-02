0bcd45169afe9bd31f056a57ff55f1323d6dccad
craig <craig.p.drummond@gmail.com>
2012-01-02 12:24:12 +0000

    Add custom icon for various artists

--- models/musiclibrarymodel.cpp
+++ models/musiclibrarymodel.cpp
@@ -141,7 +141,7 @@ QVariant MusicLibraryModel::data(const QModelIndex &index, int role) const
         switch (item->type()) {
         case MusicLibraryItem::Type_Artist: {
             MusicLibraryItemArtist *artist = static_cast<MusicLibraryItemArtist *>(item);
-            return artist->isVarious() ? QIcon::fromTheme("applications-multimedia") : QIcon::fromTheme("view-media-artist");
+            return artist->isVarious() ? QIcon::fromTheme("cantata-view-media-artist-various") : QIcon::fromTheme("view-media-artist");
         }
         case MusicLibraryItem::Type_Album:
             if (MusicLibraryItemAlbum::CoverNone==MusicLibraryItemAlbum::currentCoverSize()) {

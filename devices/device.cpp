9deb481f3a6daf783b21b65ece57f3ef5c684439
craig <craig.p.drummond@gmail.com>
2012-02-15 19:08:05 +0000

    When copying files to library, and user is a member of "audio" group - then set files/dirs as writeable and owned by "audio" group.

--- devices/device.cpp
+++ devices/device.cpp
@@ -45,8 +45,11 @@
 #include <KDE/KConfig>
 #include <KDE/KConfigGroup>
 #include <KDE/KStandardDirs>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <grp.h>
+#include <pwd.h>
 
 static QString cleanPath(const QString &path)
 {
@@ -423,22 +426,72 @@ void Device::cleanDir(const QString &dir, const QString &base, const QString &co
     }
 }
 
+// Check if $USER is in group "audio", if so we can set files to be owned by this group :-)
+static gid_t getAudioGroupId()
+{
+    static bool init=false;
+    static gid_t gid=0;
+
+    if (init) {
+        return gid;
+    }
+
+    init=true;
+
+    struct passwd *pw=getpwuid(geteuid());
+
+    if (!pw) {
+        return gid;
+    }
+
+    struct group *audioGroup=getgrnam("audio");
+
+    if (audioGroup) {
+        for (int i=0; audioGroup->gr_mem[i]; ++i) {
+            if (0==strcmp(audioGroup->gr_mem[i], pw->pw_name)) {
+                gid=audioGroup->gr_gid;
+                return gid;
+            }
+        }
+    }
+    return gid;
+}
+
+/*
+ * Set file permissions.
+ * If user is a memeber of "audio" group, then set file as owned by and writeable by "audio" group.
+ */
 void Device::setFilePerms(const QString &file)
 {
     //
     // Clear any umask before setting file perms
     mode_t oldMask(umask(0000));
-    ::chmod(QFile::encodeName(file).constData(), 0644);
+    gid_t gid=getAudioGroupId();
+    QByteArray fn=QFile::encodeName(file);
+    ::chmod(fn.constData(), 0==gid ? 0644 : 0664);
+    if (0!=gid) {
+        int rv=::chown(fn.constData(), geteuid(), gid);
+        Q_UNUSED(rv);
+    }
     // Reset umask
     ::umask(oldMask);
 }
 
+/*
+ * Create directory, and set its permissions.
+ * If user is a memeber of "audio" group, then set dir as owned by and writeable by "audio" group.
+ */
 bool Device::createDir(const QString &dir)
 {
     //
     // Clear any umask before dir is created
     mode_t oldMask(umask(0000));
-    bool status(KStandardDirs::makeDir(dir, 0755));
+    gid_t gid=getAudioGroupId();
+    bool status(KStandardDirs::makeDir(dir, 0==gid ? 0755 : 0775));
+    if (status && 0!=gid) {
+        int rv=::chown(QFile::encodeName(dir).constData(), geteuid(), gid);
+        Q_UNUSED(rv);
+    }
     // Reset umask
     ::umask(oldMask);
     return status;

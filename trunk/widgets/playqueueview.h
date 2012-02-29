bd63c8f1f0ef004092fce91f287bad037d296a3f
craig <craig@b1e49159-3266-923f-9c3e-04f16369770a>
2012-02-29 18:11:19 +0000

    Set current row

--- trunk/widgets/playqueueview.h
+++ trunk/widgets/playqueueview.h
@@ -90,7 +90,7 @@ public:
     QSet<qint32> getExpandedSongIds() const;
     void setExpanded(const QSet<quint16> &keys);
     void setFilterActive(bool f);
-    void setCurrentRow(quint32 row);
+    void setCurrentRow(qint32 row);
     void scrollTo(const QModelIndex &index, QAbstractItemView::ScrollHint hint);
     void setModel(QAbstractItemModel *m);
     void addAction(QAction *a);

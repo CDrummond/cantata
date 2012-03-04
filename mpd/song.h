ed4449d4afd2961fab247e99652658828fad04d9
craig <craig.p.drummond@gmail.com>
2012-03-04 20:58:19 +0000

    empty file == stream???

--- mpd/song.h
+++ mpd/song.h
@@ -78,7 +78,7 @@ struct Song
     bool revertVariousArtists();
     static QString capitalize(const QString &s);
     bool capitalise();
-    bool isStream() const { return /*file.isEmpty() || */file.contains("://"); }
+    bool isStream() const { return file.isEmpty() || file.contains("://"); }
 };
 
 Q_DECLARE_METATYPE(Song)

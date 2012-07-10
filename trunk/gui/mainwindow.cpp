60af7acb437a1a25a08f04d8f86770d52ea313c4
craig.p.drummond@gmail.com <craig.p.drummond@gmail.com@b1e49159-3266-923f-9c3e-04f16369770a>
2012-07-10 12:06:53 +0000

    Display version number in Qt-only about dialog.

--- trunk/gui/mainwindow.cpp
+++ trunk/gui/mainwindow.cpp
@@ -1975,7 +1975,7 @@ void MainWindow::changeConnection()
 void MainWindow::showAboutDialog()
 {
     QMessageBox::about(this, tr("About Cantata"),
-                       tr("Simple GUI front-end for MPD.<br/><br/>(c) Craig Drummond 2011-2012.<br/>Released under the GPLv2<br/><br/><i><small>Based upon QtMPC - (C) 2007-2010 The QtMPC Authors</small></i>"));
+                       tr("<b>Cantata %1</b><br/><br/>Simple GUI front-end for MPD.<br/><br/>(c) Craig Drummond 2011-2012.<br/>Released under the GPLv2<br/><br/><i><small>Based upon QtMPC - (C) 2007-2010 The QtMPC Authors</small></i>").arg(PACKAGE_VERSION));
 }
 #endif
 

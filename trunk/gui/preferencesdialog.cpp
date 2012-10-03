64077e8a7e7b0a806394f5bfb8a2efbeebd73d2a
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2012-10-03 19:16:01 +0000

    Create PageWidget class, to reduce KDe/Qt differences in preferences dialog

--- trunk/gui/preferencesdialog.cpp
+++ trunk/gui/preferencesdialog.cpp
@@ -37,50 +37,9 @@
 #include "lyricspage.h"
 #include "localize.h"
 #include "mpdconnection.h"
-#ifdef ENABLE_KDE_SUPPORT
-#include <KDE/KPageWidget>
-#include <KDE/KIcon>
-#else
-#include "fancytabwidget.h"
-#include "proxysettings.h"
-#endif
-
+#include "pagewidget.h"
 #ifndef ENABLE_KDE_SUPPORT
-class ConfigPage : public QWidget
-{
-public:
-    ConfigPage(QWidget *p, const QString &title, const QIcon &icon, QWidget *cfg) : QWidget(p)
-    {
-        static int size=-1;
-
-        if (-1==size) {
-            size=QApplication::fontMetrics().height();
-            if (size>20) {
-                size=Icon::stdSize(size*1.25);
-            } else {
-                size=22;
-            }
-        }
-
-        QBoxLayout *layout=new QBoxLayout(QBoxLayout::TopToBottom, this);
-        QBoxLayout *titleLayout=new QBoxLayout(QBoxLayout::LeftToRight, 0);
-        titleLayout->addWidget(new QLabel("<b>"+title+"</b>", this));
-        titleLayout->addItem(new QSpacerItem(16, 16, QSizePolicy::Expanding, QSizePolicy::Minimum));
-
-        QLabel *icn=new QLabel(this);
-        icn->setPixmap(icon.pixmap(size, size));
-        titleLayout->addWidget(icn);
-        layout->addLayout(titleLayout);
-        layout->addItem(new QSpacerItem(8, 8, QSizePolicy::Fixed, QSizePolicy::Fixed));
-        layout->addWidget(cfg);
-        QSizePolicy sizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
-        sizePolicy.setHorizontalStretch(0);
-        sizePolicy.setVerticalStretch(0);
-        sizePolicy.setHeightForWidth(cfg->sizePolicy().hasHeightForWidth());
-        cfg->setSizePolicy(sizePolicy);
-        cfg->setParent(this);
-    }
-};
+#include "proxysettings.h"
 #endif
 
 PreferencesDialog::PreferencesDialog(QWidget *parent, LyricsPage *lp)
@@ -88,12 +47,7 @@ PreferencesDialog::PreferencesDialog(QWidget *parent, LyricsPage *lp)
 {
     setButtons(Ok|Apply|Cancel);
 
-    #ifdef ENABLE_KDE_SUPPORT
-    KPageWidget *widget = new KPageWidget(this);
-    #else
-    FancyTabWidget *widget = new FancyTabWidget(this, false, true);
-    #endif
-
+    PageWidget *widget = new PageWidget(this);
     server = new ServerSettings(widget);
     serverplayback = new ServerPlaybackSettings(widget);
     playback = new PlaybackSettings(widget);
@@ -113,56 +67,22 @@ PreferencesDialog::PreferencesDialog(QWidget *parent, LyricsPage *lp)
     #endif
     const QList<UltimateLyricsProvider *> &lprov=lp->getProviders();
     lyrics->Load(lprov);
-    #ifdef ENABLE_KDE_SUPPORT
-    KPageWidgetItem *page=widget->addPage(server, i18n("Connection"));
-    page->setHeader(i18n("Connection Settings"));
-    page->setIcon(KIcon("network-server"));
-    page=widget->addPage(serverplayback, i18n("Output"));
-    page->setHeader(i18n("Output Settings"));
-    page->setIcon(Icon::speakerIcon);
-    page=widget->addPage(playback, i18n("Playback"));
-    page->setHeader(i18n("Playback Settings"));
-    page->setIcon(KIcon("media-playback-start"));
-    page=widget->addPage(interface, i18n("Interface"));
-    page->setHeader(i18n("Interface Settings"));
-    page->setIcon(KIcon("view-choose"));
-    page=widget->addPage(ext, i18n("External"));
-    page->setHeader(i18n("External Settings"));
-    page->setIcon(KIcon("video-display"));
+    widget->addPage(server, i18n("Connection"), Icon("network-server"), i18n("Connection Settings"));
+    widget->addPage(serverplayback, i18n("Output"), Icon::speakerIcon, i18n("Output Settings"));
+    widget->addPage(playback, i18n("Playback"), Icon("media-playback-start"), i18n("Playback Settings"));
+    widget->addPage(interface, i18n("Interface"), Icon("preferences-other"), i18n("Interface Settings"));
+    widget->addPage(ext, i18n("External"), Icon("video-display"), i18n("External Settings"));
     #ifdef TAGLIB_FOUND
-    page=widget->addPage(http, i18n("HTTP Server"));
-    page->setHeader(i18n("HTTP Server Settings"));
+    widget->addPage(http, i18n("HTTP Server"), Icon("network-wired"), i18n("HTTP Server Settings"));
     #endif
-    page->setIcon(KIcon("network-wired"));
-    page=widget->addPage(lyrics, i18n("Lyrics"));
-    page->setHeader(i18n("Lyrics Settings"));
-    page->setIcon(Icon::lyricsIcon);
-    #else
-    QIcon icon=Icon("network-server");
-    widget->AddTab(new ConfigPage(this, i18n("Connection Settings"), icon, server), icon, i18n("Connection"));
-    icon=Icon::speakerIcon;
-    widget->AddTab(new ConfigPage(this, i18n("Output Settings"), icon, serverplayback), icon, i18n("Output"));
-    icon=Icon("media-playback-start");
-    widget->AddTab(new ConfigPage(this, i18n("Playback Settings"),icon, playback), icon, i18n("Playback"));
-    icon=Icon("view-choose");
-    if (icon.isNull()) {
-        icon=Icon("preferences-other");
-    }
-    widget->AddTab(new ConfigPage(this, i18n("Interface Settings"), icon, interface), icon, i18n("Interface"));
-    icon=Icon("video-display");
-    widget->AddTab(new ConfigPage(this, i18n("External Settings"), icon, ext), icon, i18n("External"));
-    #ifdef TAGLIB_FOUND
-    icon=Icon("network-wired");
-    widget->AddTab(new ConfigPage(this, i18n("HTTP Server Settings"), icon, http), icon, i18n("HTTP Server"));
-    #endif
-    icon=Icon::lyricsIcon;
-    widget->AddTab(new ConfigPage(this, i18n("Lyrics Settings"), icon, lyrics), icon, i18n("Lyrics"));
-    proxy = new ProxySettings(this);
+    widget->addPage(lyrics, i18n("Lyrics"), Icon::lyricsIcon, i18n("Lyrics Settings"));
+
+    #ifndef ENABLE_KDE_SUPPORT
+    proxy = new ProxySettings(widget);
     proxy->load();
-    icon=Icon("preferences-system-network");
-    widget->AddTab(new ConfigPage(this, i18nc("Qt-only", "Proxy Settings"), icon, proxy), icon, i18nc("Qt-only", "Proxy"));
-    widget->SetMode(FancyTabWidget::Mode_LargeSidebar);
+    widget->addPage(proxy, i18nc("Qt-only", "Proxy"), Icon("preferences-system-network"), i18nc("Qt-only", "Proxy Settings"));
     #endif
+    widget->allPagesAdded();
     setCaption(i18n("Configure"));
     setMainWidget(widget);
     connect(server, SIGNAL(connectTo(const MPDConnectionDetails &)), SIGNAL(connectTo(const MPDConnectionDetails &)));

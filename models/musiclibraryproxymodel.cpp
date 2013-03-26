1191382599e6562957a35c1f5a6fcb46d87958d5
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2013-03-26 19:10:35 +0000

    proxy model also needs to use toItem()

--- models/musiclibraryproxymodel.cpp
+++ models/musiclibraryproxymodel.cpp
@@ -88,7 +88,7 @@ bool MusicLibraryProxyModel::filterAcceptsRow(int sourceRow, const QModelIndex &
     }
 
     const QModelIndex index = sourceModel()->index(sourceRow, 0, sourceParent);
-    const MusicLibraryItem *item = static_cast<MusicLibraryItem *>(index.internalPointer());
+    const MusicLibraryItem *item = toItem(index);
 
     if (!filterGenre.isEmpty() && !item->hasGenre(filterGenre)) {
         return false;
@@ -108,6 +108,11 @@ bool MusicLibraryProxyModel::filterAcceptsRow(int sourceRow, const QModelIndex &
     }
 
     return false;
+}
+
+const MusicLibraryItem * MusicLibraryProxyModel::toItem(const QModelIndex &idx) const
+{
+    return static_cast<MusicLibraryItem *>(idx.internalPointer());
 }
 
 bool MusicLibraryProxyModel::lessThan(const QModelIndex &left, const QModelIndex &right) const

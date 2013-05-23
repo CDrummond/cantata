55ecb8d141c7c934aa0c802513490b321333c6b7
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2013-05-23 18:43:16 +0000

    Link to QtMPC web site

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -1450,7 +1450,7 @@ void MainWindow::showAboutDialog()
 {
     QMessageBox::about(this, i18nc("Qt-only", "About Cantata"),
                        i18nc("Qt-only", "<b>Cantata %1</b><br/><br/>MPD client.<br/><br/>(c) Craig Drummond 2011-2013.<br/>Released under the GPLv3<br/><br/>"
-                             "<i><small>Based upon QtMPC - (C) 2007-2010 The QtMPC Authors<br/>"
+                             "<i><small>Based upon <a href=\"http://qtmpc.lowblog.nl\">QtMPC</a> - (C) 2007-2010 The QtMPC Authors<br/>"
                              "Context view backdrops courtesy of <a href=\"http://www.htbackdrops.com\">Home Theater Backdrops</a><br/>"
                              "Context view metadata courtesy of <a href=\"http://www.wikipedia.org\">Wikipedia</a></small></i>").arg(PACKAGE_VERSION_STRING));
 }

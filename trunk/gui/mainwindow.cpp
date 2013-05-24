0b383c9ba882767e17f18d835b85e24e41bb0f5c
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2013-05-24 19:08:46 +0000

    Link to GPL text

--- trunk/gui/mainwindow.cpp
+++ trunk/gui/mainwindow.cpp
@@ -1451,7 +1451,7 @@ void MainWindow::changeConnection()
 void MainWindow::showAboutDialog()
 {
     QMessageBox::about(this, i18nc("Qt-only", "About Cantata"),
-                       i18nc("Qt-only", "<b>Cantata %1</b><br/><br/>MPD client.<br/><br/>(c) Craig Drummond 2011-2013.<br/>Released under the GPLv3<br/><br/>"
+                       i18nc("Qt-only", "<b>Cantata %1</b><br/><br/>MPD client.<br/><br/>(c) Craig Drummond 2011-2013.<br/>Released under the <a href=\"http://www.gnu.org/licenses/gpl.html\">GPLv3</a><br/><br/>"
                              "<i><small>Based upon <a href=\"http://qtmpc.lowblog.nl\">QtMPC</a> - (C) 2007-2010 The QtMPC Authors<br/>"
                              "Context view backdrops courtesy of <a href=\"http://www.htbackdrops.com\">Home Theater Backdrops</a><br/>"
                              "Context view metadata courtesy of <a href=\"http://www.wikipedia.org\">Wikipedia</a></small></i>").arg(PACKAGE_VERSION_STRING));

4aad69af04af2503a8cb423dad0714d6346f4e16
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2013-06-06 18:45:01 +0000

    Add event filter to capture short-cut events, so that these can be passed onto buddy

--- support/buddylabel.h
+++ support/buddylabel.h
@@ -25,65 +25,17 @@
 #define BUDDYLABEL_H
 
 #include <QLabel>
-#include <QCheckBox>
-#include <QComboBox>
-#include "spinbox.h"
-#ifndef ENABLE_KDE_SUPPORT
-#include "pathrequester.h"
-#endif
 
 class BuddyLabel : public QLabel
 {
 public:
-    BuddyLabel(const QString &text, QWidget *p, QWidget *b=0)
-        : QLabel(text, p) {
-        if (b) {
-            setBuddy(b);
-        }
-    }
-
-    BuddyLabel(QWidget *p, QWidget *b=0)
-        : QLabel(p) {
-        if (b) {
-            setBuddy(b);
-        }
-    }
-
-    virtual ~BuddyLabel() {
-    }
+    BuddyLabel(const QString &text, QWidget *p, QWidget *b=0);
+    BuddyLabel(QWidget *p, QWidget *b=0);
+    virtual ~BuddyLabel() { }
 
 protected:
-    void mouseReleaseEvent(QMouseEvent *) {
-        if (buddy() && buddy()->isEnabled()) {
-            #ifndef ENABLE_KDE_SUPPORT
-            PathRequester *pr=qobject_cast<PathRequester*>(buddy());
-            if (pr) {
-                pr->setFocus();
-                return;
-            }
-            #endif
-            #ifndef Q_WS_WIN
-            SpinBox *sb=qobject_cast<SpinBox*>(buddy());
-            if (sb) {
-                sb->setFocus();
-                return;
-            }
-            #endif
-
-            buddy()->setFocus();
-
-            QCheckBox *cb=qobject_cast<QCheckBox*>(buddy());
-            if (cb) {
-                cb->setChecked(!cb->isChecked());
-                return;
-            }
-            QComboBox *combo=qobject_cast<QComboBox*>(buddy());
-            if (combo) {
-                combo->showPopup();
-                return;
-            }
-        }
-    }
+    bool event(QEvent *e);
+    void mouseReleaseEvent(QMouseEvent *);
 };
 
 #endif

bac6021fd54fa498ac14c2135eb06dea2baadef5
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2013-06-08 15:12:18 +0000

    Fix KDE compile

--- gui/main.cpp
+++ gui/main.cpp
@@ -27,7 +27,7 @@
 #include <KDE/KAboutData>
 #include <KDE/KCmdLineArgs>
 #include <KDE/KStartupInfo>
-#include "contextpage.h"
+#include "contextwidget.h"
 #else
 #include <QTranslator>
 #include <QTextCodec>
@@ -101,7 +101,7 @@ int main(int argc, char *argv[])
     aboutData.addAuthor(ki18n("Roeland Douma"), ki18n("QtMPC author"), QByteArray(), "http://qtmpc.lowblog.nl");
     aboutData.addAuthor(ki18n("Daniel Selinger"), ki18n("QtMPC author"), QByteArray(), "http://qtmpc.lowblog.nl");
     aboutData.addAuthor(ki18n("Armin Walland"), ki18n("QtMPC author"), QByteArray(), "http://qtmpc.lowblog.nl");
-    if (ContextPage::constApiKey.latin1()) {
+    if (ContextWidget::constApiKey.latin1()) {
         aboutData.addCredit(ki18n("Home Theater Backdrops"), ki18n("Context view backdrops"), QByteArray(), "www.htbackdrops.com");
     }
     aboutData.addCredit(ki18n("Wikipedia"), ki18n("Context view metadata"), QByteArray(), "www.wikipedia.org");

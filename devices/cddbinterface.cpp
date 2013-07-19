55df9c55bb11825c9f859a503919b733018be920
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2013-07-19 18:28:04 +0000

    Use "Track N" for unknown tracks

--- devices/cddbinterface.cpp
+++ devices/cddbinterface.cpp
@@ -159,7 +159,7 @@ void CddbInterface::readDisc()
                     cddb_track_t *track = cddb_track_new();
                     if (track) {
                         cddb_track_set_frame_offset(track, te.entry.addr.lba + SECONDS_TO_FRAMES(2));
-                        cddb_track_set_title(track, te.entry.control&0x04 ? dataTrack().toUtf8().constData() : unknown.constData());
+                        cddb_track_set_title(track, te.entry.control&0x04 ? dataTrack().toUtf8().constData() : i18n("Track %1", i).toUtf8().constData());
                         cddb_track_set_artist(track, unknown.constData());
                         cddb_disc_add_track(disc, track);
                     }
@@ -185,7 +185,7 @@ void CddbInterface::readDisc()
                     cddb_track_t *track = cddb_track_new();
                     if (track) {
                         cddb_track_set_frame_offset(track, te.cdte_addr.lba + SECONDS_TO_FRAMES(2));
-                        cddb_track_set_title(track, te.cdte_ctrl&CDROM_DATA_TRACK ? dataTrack().toUtf8().constData() : unknown.constData());
+                        cddb_track_set_title(track, te.cdte_ctrl&CDROM_DATA_TRACK ? dataTrack().toUtf8().constData() : i18n("Track %1", i).toUtf8().constData());
                         cddb_track_set_artist(track, unknown.constData());
                         cddb_disc_add_track(disc, track);
                     }

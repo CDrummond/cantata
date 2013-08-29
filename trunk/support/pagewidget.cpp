bfdcecb63b0ac894f8b59aee9fd73b814303efd2
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2013-08-29 18:25:04 +0000

    Use QListWidget in Qt preferences dialog, so that keyboard navigation can be used.

--- trunk/support/pagewidget.cpp
+++ trunk/support/pagewidget.cpp
@@ -23,10 +23,200 @@
 
 #include "pagewidget.h"
 #include "icon.h"
+#include "gtkstyle.h"
+#include <QListWidget>
+#include <QStackedWidget>
 #include <QBoxLayout>
 #include <QLabel>
 #include <QSizePolicy>
 #include <QApplication>
+#include <QFontMetrics>
+#include <QTextLayout>
+#include <QPainter>
+
+static int layoutText(QTextLayout *layout, int maxWidth)
+{
+    qreal height = 0;
+    int textWidth = 0;
+    layout->beginLayout();
+    while (true) {
+        QTextLine line = layout->createLine();
+        if (!line.isValid()) {
+            break;
+        }
+        line.setLineWidth(maxWidth);
+        line.setPosition(QPointF(0, height));
+        height += line.height();
+        textWidth = qMax(textWidth, qRound(line.naturalTextWidth() + 0.5));
+    }
+    layout->endLayout();
+    return textWidth;
+}
+
+class PageWidgetItemDelegate : public QAbstractItemDelegate
+{
+public:
+    PageWidgetItemDelegate(QObject *parent)
+        : QAbstractItemDelegate(parent)
+    {
+        int height=QApplication::fontMetrics().height();
+        iconSize=height>22 ? Icon::stdSize(height*2.5) : 32;
+    }
+
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+    {
+        if (!index.isValid()) {
+            return;
+        }
+        bool mouseOver=option.state&QStyle::State_MouseOver;
+        bool gtk=mouseOver && GtkStyle::isActive();
+        bool selected=option.state&QStyle::State_Selected;
+        bool drawBgnd=true;
+
+        if (!underMouse) {
+            if (mouseOver && !selected) {
+                drawBgnd=false;
+            }
+            mouseOver=false;
+        }
+
+        const QString text = index.model()->data(index, Qt::DisplayRole).toString();
+        const QIcon icon = index.model()->data(index, Qt::DecorationRole).value<QIcon>();
+        const QPixmap pixmap = icon.pixmap(iconSize, iconSize);
+
+        QFontMetrics fm = painter->fontMetrics();
+        int wp = pixmap.width();
+        int hp = pixmap.height();
+
+        QTextLayout iconTextLayout(text, option.font);
+        QTextOption textOption(Qt::AlignHCenter);
+        iconTextLayout.setTextOption(textOption);
+        int maxWidth = qMax(3 * wp, 8 * fm.height());
+        layoutText(&iconTextLayout, maxWidth);
+
+        QPen pen = painter->pen();
+        QPalette::ColorGroup cg = option.state & QStyle::State_Enabled
+                ? QPalette::Normal : QPalette::Disabled;
+        if (cg == QPalette::Normal && !(option.state & QStyle::State_Active)) {
+            cg = QPalette::Inactive;
+        }
+
+        QStyleOptionViewItemV4 opt(option);
+        opt.showDecorationSelected = true;
+        QStyle *style = opt.widget ? opt.widget->style() : QApplication::style();
+
+        if (drawBgnd) {
+            if (mouseOver && gtk) {
+                GtkStyle::drawSelection(opt, painter, selected ? 0.75 : 0.25);
+            } else {
+                style->drawPrimitive(QStyle::PE_PanelItemViewItem, &opt, painter, opt.widget);
+            }
+        }
+
+        if (selected) {
+            painter->setPen(option.palette.color(cg, QPalette::HighlightedText));
+        } else {
+            painter->setPen(option.palette.color(cg, QPalette::Text));
+        }
+
+        painter->drawPixmap(option.rect.x() + (option.rect.width()/2)-(wp/2), option.rect.y() + 5, pixmap);
+        if (!text.isEmpty()) {
+            iconTextLayout.draw(painter, QPoint(option.rect.x() + (option.rect.width()/2)-(maxWidth/2), option.rect.y() + hp+7));
+        }
+        painter->setPen(pen);
+        drawFocus(painter, option, option.rect);
+    }
+
+    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
+    {
+        if (!index.isValid()) {
+            return QSize(0, 0);
+        }
+
+        const QString text = index.model()->data(index, Qt::DisplayRole).toString();
+        const QIcon icon = index.model()->data(index, Qt::DecorationRole).value<QIcon>();
+        const QPixmap pixmap = icon.pixmap(iconSize, iconSize);
+
+        QFontMetrics fm = option.fontMetrics;
+        int gap = fm.height();
+        int wp = pixmap.width();
+        int hp = pixmap.height();
+
+        if (hp == 0) {
+            /**
+            * No pixmap loaded yet, we'll use the default icon size in this case.
+            */
+            hp = iconSize;
+            wp = iconSize;
+        }
+
+        QTextLayout iconTextLayout(text, option.font);
+        int wt = layoutText(&iconTextLayout, qMax(3 * wp, 8 * fm.height()));
+        int ht = iconTextLayout.boundingRect().height();
+
+        int width, height;
+        if (text.isEmpty()) {
+            height = hp;
+        } else {
+            height = hp + ht + 10;
+
+        }
+
+        width = qMax(wt, wp) + gap;
+        return QSize(width, height);
+    }
+
+    void drawFocus(QPainter *painter, const QStyleOptionViewItem &option, const QRect &rect) const
+    {
+        if (option.state & QStyle::State_HasFocus) {
+            QStyleOptionFocusRect o;
+            o.QStyleOption::operator=(option);
+            o.rect = rect;
+            o.state |= QStyle::State_KeyboardFocusChange;
+            QPalette::ColorGroup cg = (option.state & QStyle::State_Enabled)
+                    ? QPalette::Normal : QPalette::Disabled;
+            o.backgroundColor = option.palette.color(cg, (option.state & QStyle::State_Selected)
+                                                      ? QPalette::Highlight : QPalette::Background);
+            QApplication::style()->drawPrimitive(QStyle::PE_FrameFocusRect, &o, painter);
+        }
+    }
+
+    void setUnderMouse(bool u) { underMouse=u; }
+
+private:
+    int iconSize;
+    bool underMouse;
+};
+
+class PageWidgetViewEventHandler : public QObject
+{
+public:
+    PageWidgetViewEventHandler(PageWidgetItemDelegate *d, QListWidget *v)
+        : QObject(v)
+        , delegate(d)
+        , view(v)
+    {
+    }
+
+protected:
+    bool eventFilter(QObject *obj, QEvent *event)
+    {
+        if (delegate) {
+            if (QEvent::Enter==event->type()) {
+                delegate->setUnderMouse(true);
+                view->viewport()->update();
+            } else if (QEvent::Leave==event->type()) {
+                delegate->setUnderMouse(false);
+                view->viewport()->update();
+            }
+        }
+        return QObject::eventFilter(obj, event);
+    }
+
+protected:
+    PageWidgetItemDelegate *delegate;
+    QListWidget *view;
+};
 
 PageWidgetItem::PageWidgetItem(QWidget *p, const QString &header, const Icon &icon, QWidget *cfg)
     : QWidget(p) {
@@ -58,20 +248,65 @@ PageWidgetItem::PageWidgetItem(QWidget *p, const QString &header, const Icon &ic
 }
 
 PageWidget::PageWidget(QWidget *p)
-    : FancyTabWidget(p, false, true)
+    : QWidget(p)
 {
+    QBoxLayout *layout=new QBoxLayout(QBoxLayout::LeftToRight, this);
+    list = new QListWidget(p);
+    stack = new QStackedWidget(p);
+    connect(list, SIGNAL(currentRowChanged(int)), stack, SLOT(setCurrentIndex(int)));
+    layout->addWidget(list);
+    layout->addWidget(stack);
+    layout->setMargin(0);
+    list->setViewMode(QListView::ListMode);
+    list->setVerticalScrollMode(QListView::ScrollPerPixel);
+    list->setMovement(QListView::Static);
+    PageWidgetItemDelegate *delegate=new PageWidgetItemDelegate(list);
+    list->setItemDelegate(delegate);
+    list->setSelectionBehavior(QAbstractItemView::SelectItems);
+    list->setSelectionMode(QAbstractItemView::SingleSelection);
+    list->setAttribute(Qt::WA_MouseTracking);
+    list->installEventFilter(new PageWidgetViewEventHandler(delegate, list));
 }
 
 PageWidgetItem * PageWidget::addPage(QWidget *widget, const QString &name, const Icon &icon, const QString &header)
 {
-    PageWidgetItem *item=new PageWidgetItem(parentWidget(), header, icon, widget);
-    AddTab(item, icon, name);
-    return item;
+    PageWidgetItem *page=new PageWidgetItem(stack, header, icon, widget);
+    QListWidgetItem *listItem=new QListWidgetItem(name, list);
+    listItem->setIcon(icon);
+    stack->addWidget(page);
+    list->addItem(listItem);
+
+    int rows = list->model()->rowCount();
+    int width = 0;
+    int height = 0;
+    for (int i = 0; i < rows; ++i) {
+        QSize rowSize=list->sizeHintForIndex(list->model()->index(i, 0));
+        width = qMax(width, rowSize.width());
+        height += rowSize.height();
+    }
+    width+=25;
+    list->setFixedWidth(width);
+    list->setMinimumHeight(height);
+
+    QSize stackSize = stack->size();
+    for (int i = 0; i < stack->count(); ++i) {
+        const QWidget *widget = stack->widget(i);
+        if (widget) {
+            stackSize = stackSize.expandedTo(widget->minimumSizeHint());
+        }
+    }
+    stack->setMinimumSize(stackSize);
+
+    QSize sz=size();
+    sz=sz.expandedTo(stackSize);
+    sz=sz.expandedTo(QSize(width, height));
+    setMinimumSize(sz);
+    list->setCurrentRow(0);
+    stack->setCurrentIndex(0);
+    return page;
 }
 
-void PageWidget::allPagesAdded()
+int PageWidget::count()
 {
-    SetMode(FancyTabWidget::Mode_LargeSidebar);
-    adjustSize();
-    setMinimumHeight(count()*tabSize().height());
+    return list->count();
 }

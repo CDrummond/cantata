a85f588f3fa48b437bec0b21dd77a1a367470664
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2013-09-20 19:55:00 +0000

    Need to sort online when add podcast

--- online/onlineservice.cpp
+++ online/onlineservice.cpp
@@ -413,6 +413,11 @@ void OnlineService::emitDataChanged(const QModelIndex &idx)
     emit static_cast<OnlineServicesModel *>(m_model)->dataChanged(idx, idx);
 }
 
+void OnlineService::emitNeedToSort()
+{
+    emit static_cast<OnlineServicesModel *>(m_model)->needToSort();
+}
+
 void OnlineService::setBusy(bool b)
 {
     static_cast<OnlineServicesModel *>(m_model)->setBusy(id(), b);

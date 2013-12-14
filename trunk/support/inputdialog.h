0c3ac81ca638f8bab727bedc68282c031312b5b9
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2013-12-14 12:44:00 +0000

    Better Qt input dialogs - need clear button in text input.

--- trunk/support/inputdialog.h
+++ trunk/support/inputdialog.h
@@ -24,28 +24,42 @@
 #ifndef INPUT_DIALOG_H
 #define INPUT_DIALOG_H
 
-#include <QInputDialog>
-#ifdef ENABLE_KDE_SUPPORT
-#include <KDE/KInputDialog>
-#endif
+#include <QLineEdit>
 #include "localize.h"
+#include "dialog.h"
 
-namespace InputDialog
+class SpinBox;
+class LineEdit;
+
+class InputDialog : public Dialog
 {
-    inline QString getText(const QString &caption, const QString &label, const QString &value=QString(), bool *ok=0, QWidget *parent=0) {
-        #ifdef ENABLE_KDE_SUPPORT
-        return KInputDialog::getText(caption, label, value, ok, parent=0);
-        #else
-        return QInputDialog::getText(parent, caption, label, QLineEdit::Normal, value, ok);
-        #endif
-    }
+    Q_OBJECT
 
-    extern int getInteger(const QString &caption, const QString &label, int value=0, int minValue=INT_MIN, int maxValue=INT_MAX,
+public:
+    InputDialog(const QString &caption, const QString &label, const QString &value, QLineEdit::EchoMode echo, QWidget *parent);
+    InputDialog(const QString &caption, const QString &label, int value, int minValue, int maxValue, int step, QWidget *parent);
+
+    static QString getText(const QString &caption, const QString &label, QLineEdit::EchoMode echoMode, const QString &value=QString(),
+                           bool *ok=0, QWidget *parent=0);
+    static int getInteger(const QString &caption, const QString &label, int value=0, int minValue=INT_MIN, int maxValue=INT_MAX,
                           int step=1, int base=10, bool *ok=0, QWidget *parent=0);
 
-    inline QString getPassword(const QString &value=QString(), bool *ok=0, QWidget *parent=0) {
-        return QInputDialog::getText(parent, i18n("Password"), i18n("Please enter password:"), QLineEdit::Password, value, ok);
+    static QString getText(const QString &caption, const QString &label, const QString &value=QString(), bool *ok=0, QWidget *parent=0) {
+        return getText(caption, label, QLineEdit::Normal, value, ok, parent);
     }
+    static QString getPassword(const QString &value=QString(), bool *ok=0, QWidget *parent=0) {
+        return getText(i18n("Password"), i18n("Please enter password:"), QLineEdit::Password, value, ok, parent);
+    }
+
+private:
+    void init(bool intInput, const QString &caption, const QString &label);
+
+private Q_SLOTS:
+    void enableOkButton();
+
+private:
+    SpinBox *spin;
+    LineEdit *edit;
 };
 
 #endif // INPUT_DIALOG_H

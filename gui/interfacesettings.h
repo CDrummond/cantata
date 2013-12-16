e01eaaf00132611dd03ae19958d6973568607365
craig.p.drummond <craig.p.drummond@gmail.com>
2013-12-16 19:28:06 +0000

    Add option to control startup-state; visible, hidden, or remebered from previous run. BUG: 337

--- gui/interfacesettings.h
+++ gui/interfacesettings.h
@@ -61,6 +61,7 @@ private Q_SLOTS:
     void playListsStyleChanged();
     void playQueueGroupedChanged();
     void forceSingleClickChanged();
+    void enableStartupState();
 
 private:
     int enabledViews;

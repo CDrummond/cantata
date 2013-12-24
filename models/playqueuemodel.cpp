982ad4934a114d8744dbc03681cfc02e3c79a102
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2013-12-24 20:01:09 +0000

    Also remove CDDA files when Cantata exits.

--- models/playqueuemodel.cpp
+++ models/playqueuemodel.cpp
@@ -829,7 +829,7 @@ void PlayQueueModel::removeCantataStreams()
 {
     QList<qint32> ids;
     foreach (const Song &s, songs) {
-        if (s.isCantataStream()) {
+        if (s.isCantataStream() || s.isCdda()) {
             ids.append(s.id);
         }
     }

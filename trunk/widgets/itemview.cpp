615ad9237ee2fb92a88a79ea650e9bd4ed0dab61
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2014-02-04 19:13:42 +0000

    Remove typo

--- trunk/widgets/itemview.cpp
+++ trunk/widgets/itemview.cpp
@@ -426,7 +426,7 @@ public:
                 if (text.count()>1) {
                     int mainWidth=textMetrics.width(str);
                     text.takeFirst();
-                    str=text.join(" -tableView ");
+                    str=text.join(" - ");
                     textRect=QRect(r.x()+(mainWidth+8), r.y()+((r.height()-textHeight)/2), r.width()-(mainWidth+8), textHeight);
                     if (textRect.width()>4) {
                         str = textMetrics.elidedText(str, Qt::ElideRight, textRect.width(), QPalette::WindowText);

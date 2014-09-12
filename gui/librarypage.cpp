22698f5972a889e41664edcb27d69a19bb55fc3d
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2014-09-12 17:51:29 +0000

    Need to enable edit tags even if we dont have replaygain support.

--- gui/librarypage.cpp
+++ gui/librarypage.cpp
@@ -304,9 +304,9 @@ void LibraryPage::controlActions()
     StdActions::self()->addToStoredPlaylistAction->setEnabled(enable);
     #ifdef TAGLIB_FOUND
     StdActions::self()->organiseFilesAction->setEnabled(enable && MPDConnection::self()->getDetails().dirReadable);
+    StdActions::self()->editTagsAction->setEnabled(StdActions::self()->organiseFilesAction->isEnabled());
     #ifdef ENABLE_REPLAYGAIN_SUPPORT
     StdActions::self()->replaygainAction->setEnabled(StdActions::self()->organiseFilesAction->isEnabled());
-    StdActions::self()->editTagsAction->setEnabled(StdActions::self()->organiseFilesAction->isEnabled());
     #endif
     #ifdef ENABLE_DEVICES_SUPPORT
     StdActions::self()->deleteSongsAction->setEnabled(StdActions::self()->organiseFilesAction->isEnabled());

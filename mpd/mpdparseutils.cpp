75c305e23d202e9996038ba1cafbbc1b701d9662
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2014-10-05 08:10:59 +0000

    Detect if local file is actually a podcast

--- mpd/mpdparseutils.cpp
+++ mpd/mpdparseutils.cpp
@@ -51,6 +51,7 @@
 #include "mpdconnection.h"
 #ifdef ENABLE_ONLINE_SERVICES
 #include "online/onlineservice.h"
+#include "online/podcastservice.h"
 #endif
 
 #include <QDebug>
@@ -403,6 +404,14 @@ Song MPDParseUtils::parseSong(const QList<QByteArray> &lines, Location location)
         }
         song.file=origFile;
         song.setKey(location);
+
+        #ifdef ENABLE_ONLINE_SERVICES
+        // Check for downloaded podcasts played via local file playback
+        if (Song::OnlineSvrTrack!=song.type && PodcastService::isPodcastFile(song.file)) {
+            song.setIsFromOnlineService(PodcastService::constName);
+            song.albumartist=song.artist=PodcastService::constName;
+        }
+        #endif
     }
     return song;
 }

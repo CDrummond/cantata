cc7105c1e432e8361dea245f09d18488dce0691b
craig.p.drummond <craig.p.drummond@b1e49159-3266-923f-9c3e-04f16369770a>
2014-11-21 18:12:21 +0000

    Add config item to set music root folder

--- mpd/mpdconnection.h
+++ mpd/mpdconnection.h
@@ -160,6 +160,7 @@ struct MPDConnectionDetails {
     quint16 port;
     QString password;
     QString dir;
+    QString topLevel;
     bool dirReadable;
     QString coverName;
     #ifdef ENABLE_HTTP_STREAM_PLAYBACK

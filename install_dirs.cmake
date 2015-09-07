e2b44854fbb6b8ab2058bec85a5877f7904e891b
Craig Drummond <craig.p.drummond@gmail.com>
2015-09-07 21:25:15 +0100

    Add a uninstall target for Linux builds.

---
+++ install_dirs.cmake
@@ -0,0 +1,14 @@
+@CMAKE_INSTALL_PREFIX@/share/cantata/config
+@CMAKE_INSTALL_PREFIX@/share/cantata/icons
+@CMAKE_INSTALL_PREFIX@/share/cantata/mpd
+@CMAKE_INSTALL_PREFIX@/share/cantata/scripts
+@CMAKE_INSTALL_PREFIX@/share/cantata/themes
+@CMAKE_INSTALL_PREFIX@/share/cantata/translations
+@CMAKE_INSTALL_PREFIX@/share/cantata
+@CMAKE_INSTALL_PREFIX@/icons/ubuntu-mono-light/apps/22
+@CMAKE_INSTALL_PREFIX@/icons/ubuntu-mono-light/apps
+@CMAKE_INSTALL_PREFIX@/icons/ubuntu-mono-light
+@CMAKE_INSTALL_PREFIX@/icons/ubuntu-mono-dark/apps/22
+@CMAKE_INSTALL_PREFIX@/icons/ubuntu-mono-dark/apps
+@CMAKE_INSTALL_PREFIX@/icons/ubuntu-mono-dark
+@CMAKE_INSTALL_PREFIX@/lib/cantata

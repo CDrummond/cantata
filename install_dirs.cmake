5c6bcac12ca44eca6eeb5f528ce3b267f8e706d9
Heiko Becker <craig.p.drummond@gmail.com>
2015-11-26 19:52:45 +0100

    Allow changing the location of arch-independent data
    This is helpful on a multiarch layout where the prefix is /usr/${host}
    but arch-independent files should still be installed to /usr/share.

--- install_dirs.cmake
+++ install_dirs.cmake
@@ -1,10 +1,10 @@
-@CMAKE_INSTALL_PREFIX@/share/cantata/config
-@CMAKE_INSTALL_PREFIX@/share/cantata/icons
-@CMAKE_INSTALL_PREFIX@/share/cantata/mpd
-@CMAKE_INSTALL_PREFIX@/share/cantata/scripts
-@CMAKE_INSTALL_PREFIX@/share/cantata/themes
-@CMAKE_INSTALL_PREFIX@/share/cantata/translations
-@CMAKE_INSTALL_PREFIX@/share/cantata
+@SHARE_INSTALL_PREFIX@/cantata/config
+@SHARE_INSTALL_PREFIX@/cantata/icons
+@SHARE_INSTALL_PREFIX@/cantata/mpd
+@SHARE_INSTALL_PREFIX@/cantata/scripts
+@SHARE_INSTALL_PREFIX@/cantata/themes
+@SHARE_INSTALL_PREFIX@/cantata/translations
+@SHARE_INSTALL_PREFIX@/cantata
 @CMAKE_INSTALL_PREFIX@/icons/ubuntu-mono-light/apps/22
 @CMAKE_INSTALL_PREFIX@/icons/ubuntu-mono-light/apps
 @CMAKE_INSTALL_PREFIX@/icons/ubuntu-mono-light

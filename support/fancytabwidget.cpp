705648420afaa5b08fefd25b83d03cb75369c2be
Craig Drummond <craig.p.drummond@gmail.com>
2016-01-08 18:55:13 +0000

    Update (c) year

--- support/fancytabwidget.cpp
+++ support/fancytabwidget.cpp
@@ -1,7 +1,7 @@
 /*
  * Cantata
  *
- * Copyright (c) 2011-2015 Craig Drummond <craig.p.drummond@gmail.com>
+ * Copyright (c) 2011-2016 Craig Drummond <craig.p.drummond@gmail.com>
  */
 
 /**************************************************************************

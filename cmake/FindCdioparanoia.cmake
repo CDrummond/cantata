1e9f0fa780560c3652133961c122e220aaf1b017
Craig Drummond <craig.p.drummond@gmail.com>
2017-06-06 17:48:25 +0100

    Check for cdio/paranoia.h in cdio/paranoia/paranoia.h Issue #1022

--- cmake/FindCdioparanoia.cmake
+++ cmake/FindCdioparanoia.cmake
@@ -6,12 +6,28 @@
 # CDIOPARANOIA_LIBRARIES - The libcdio_paranoia libraries
 
 include(FindPkgConfig)
+include(CheckIncludeFiles)
 
 if(PKG_CONFIG_FOUND)
   pkg_check_modules (CDIOPARANOIA libcdio_paranoia)
   list(APPEND CDIOPARANOIA_INCLUDE_DIRS ${CDIOPARANOIA_INCLUDEDIR})
 endif()
 
+if (CDIOPARANOIA_FOUND)
+    check_include_files(cdio/paranoia.h HAVE_CDIO_PARANOIA_H)
+    check_include_files(cdio/cdda.h HAVE_CDIO_CDDA_H)
+    # Issue 1022 - sometimes its cdio/paranoia/paranoia.h
+    if (NOT HAVE_CDIO_PARANOIA_H)
+        check_include_files(cdio/paranoia/paranoia.h HAVE_CDIO_PARANOIA_PARANOIA_H)
+    endif()
+    if (NOT HAVE_CDIO_CDDA_H)
+        check_include_files(cdio/paranoia/cdda.h HAVE_CDIO_PARANOIA_CDDA_H)
+    endif()
+    if (NOT HAVE_CDIO_PARANOIA_H AND NOT HAVE_CDIO_PARANOIA_PARANOIA_H AND NOT HAVE_CDIO_CDDA_H AND NOT HAVE_CDIO_PARANOIA_CDDA_H)
+        set(CDIOPARANOIA_FOUND OFF)
+        message("Failed to determine cdio/paranoia header file location")
+    endif()
+endif()
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(CDIOPARANOIA DEFAULT_MSG CDIOPARANOIA_INCLUDE_DIRS CDIOPARANOIA_LIBRARIES)
 

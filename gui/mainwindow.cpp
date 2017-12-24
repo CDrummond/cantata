0b37f7475aa5bdaec6db74d77e567dc9b1b650c7
Craig Drummond <craig.p.drummond@gmail.com>
2017-12-24 15:00:13 +0000

    Remove menubar? Issue #1147

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -637,6 +637,12 @@ void MainWindow::init()
     mainMenu->addAction(quitAction);
     menuButton->setIcon(Icons::self()->toolbarMenuIcon);
     menuButton->setAlignedMenu(mainMenu);
+
+    // Ensure no menubar? Issue #1147
+    QMenuBar *mb = menuBar();
+    if (mb) {
+        mb->deleteLater();
+    }
     #endif
 
     dynamicLabel->setVisible(false);

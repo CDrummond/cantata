2bd461756ae68c3e0cc58e9d983a729e0b124c32
Craig Drummond <craig.p.drummond@gmail.com>
2018-02-05 20:05:32 +0000

    Tags not needed to serve files?

--- http/httpserver.cpp
+++ http/httpserver.cpp
@@ -235,7 +235,7 @@ QByteArray HttpServer::encodeUrl(const QString &file)
     #endif
     s.file=file;
     #endif
-    return s.isEmpty() ? QByteArray() : encodeUrl(s);
+    return /*s.isEmpty() ? QByteArray() :*/ encodeUrl(s);
 }
 
 Song HttpServer::decodeUrl(const QString &url) const

d8de6dae90af2ead8c47a95df359ad10819c26bc
Craig Drummond <craig.p.drummond@gmail.com>
2019-09-15 15:56:58 +0100

    Update (c) year

--- translations/cantata_fr.ts
+++ translations/cantata_fr.ts
@@ -8766,8 +8766,8 @@ Si cette recherche donne de nouveaux résultats, ils seront toujours associés a
     </message>
     <message>
         <location filename="../gui/mainwindow.cpp" line="1212"/>
-        <source>&lt;b&gt;Cantata %1&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;MPD client.&lt;br/&gt;&lt;br/&gt;&amp;copy; 2011-2018 Craig Drummond&lt;br/&gt;Released under the &lt;a href=&quot;http://www.gnu.org/licenses/gpl.html&quot;&gt;GPLv3&lt;/a&gt;</source>
-        <translation>&lt;b&gt;Cantata %1&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;client MPD.&lt;br/&gt;&lt;br/&gt;&amp;copy; 2011-2018 Craig Drummond&lt;br/&gt;publié sous la &lt;a href=&quot;http://www.gnu.org/licenses/gpl.html&quot;&gt;GPLv3&lt;/a&gt;</translation>
+        <source>&lt;b&gt;Cantata %1&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;MPD client.&lt;br/&gt;&lt;br/&gt;&amp;copy; 2011-2019 Craig Drummond&lt;br/&gt;Released under the &lt;a href=&quot;http://www.gnu.org/licenses/gpl.html&quot;&gt;GPLv3&lt;/a&gt;</source>
+        <translation>&lt;b&gt;Cantata %1&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;client MPD.&lt;br/&gt;&lt;br/&gt;&amp;copy; 2011-2019 Craig Drummond&lt;br/&gt;publié sous la &lt;a href=&quot;http://www.gnu.org/licenses/gpl.html&quot;&gt;GPLv3&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../gui/mainwindow.cpp" line="1215"/>

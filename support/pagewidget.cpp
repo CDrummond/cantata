195b66277c7283e7df4df5f217b3b6b9a492fd61
John Regan <john@jrjrtech.com>
2021-07-20 15:26:25 -0400

    fix deprecation: QPalette::Background obsolete

--- support/pagewidget.cpp
+++ support/pagewidget.cpp
@@ -227,7 +227,7 @@ public:
             QPalette::ColorGroup cg = (option.state & QStyle::State_Enabled)
                     ? QPalette::Normal : QPalette::Disabled;
             o.backgroundColor = option.palette.color(cg, (option.state & QStyle::State_Selected)
-                                                      ? QPalette::Highlight : QPalette::Background);
+                                                      ? QPalette::Highlight : QPalette::Window);
             QApplication::style()->drawPrimitive(QStyle::PE_FrameFocusRect, &o, painter);
         }
     }
